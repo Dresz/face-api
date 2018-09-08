32fffdb52ca09c198e1e4ff24f62876102f70788
Dresz <43056279+Dresz@users.noreply.github.com>
2018-09-07 23:47:53 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
-
-
-import(
-    "fmt",
-    "log",
-    "net/http"
+import (
+  "fmt"
+  "net/http"
+	"io/ioutil"
+  "strings"
+  "google.golang.org/appengine"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+  url := "https://graph.facebook.com/v3.1/685148168507218/feed?"
+
+	payload := strings.NewReader("message=Alerta%20la%20Casa%20se%20esta%20quemando&access_token=EAADNGF3mxwMBAC8VNUbnyrihiXpM2nQ5m8avo3LdZCPSZCnpy469HSwOVkoFaz9S4S5L0w8I7w4y4xCgZAylgQ1B7ukislASxeq1zOVsohmFUWGoi42K8DWRP4j8fh8Ht3ZCOg2uYIBWJEvQKTl1ZA8ZCBTaCgRCSkK4qqtdlEOQZDZD")
+
+	req, err := http.NewRequest("POST", url, payload)
+
+  if err != nil {
+    fmt.Fprint(w, "Fallo req!")
+    return
+  }
+  client := http.Client{}
+  resp, err :=  client.Do(req)
+  if err != nil {
+    fmt.Fprint(w, "Fallo resp!")
+    return
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+
+  fmt.Printf("%s\n", string(body))
+	fmt.Fprint(w, "Post enviado!")
+}
 func main() {
-        http.HandleFunc("/", handle)
-        http.HandleFunc("/_ah/health", healthCheckHandler)
-        log.Print("Listening on port 8080")
-        log.Fatal(http.ListenAndServe(":8080", nil))
+  http.HandleFunc("/", handler) // Set endpoint handler
+  appengine.Main() // Start the server
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path != "/" {
-                http.NotFound(w, r)
-                return
-        }
-        fmt.Fprint(w, "Hello world!")
-}
+//EAADNGF3mxwMBAC8VNUbnyrihiXpM2nQ5m8avo3LdZCPSZCnpy469HSwOVkoFaz9S4S5L0w8I7w4y4xCgZAylgQ1B7ukislASxeq1zOVsohmFUWGoi42K8DWRP4j8fh8Ht3ZCOg2uYIBWJEvQKTl1ZA8ZCBTaCgRCSkK4qqtdlEOQZDZD
+//EAADNGF3mxwMBACbC1RdceaKTO4gE6z3sp9zv4lZCZA0BYdtPwulhkmgVOZB3JbFqmdqpWwiLg0bLnReq3uuJUdIMGHcZAH0Qqo62Vt2ZBQ42UiZCSzhZBP3ApgRJFBdSLwMKKmd5T2p5CXZCd7JJjVD7jurdmNtlOnYZD
+/*
 
-func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "ok")
-} 
-
+curl -i -X POST \
+ "https://graph.facebook.com/v3.1/685148168507218/feed?message=Alerta%20la%20Casa%20se%20esta%20quemando&access_token=EAADNGF3mxwMBAC8VNUbnyrihiXpM2nQ5m8avo3LdZCPSZCnpy469HSwOVkoFaz9S4S5L0w8I7w4y4xCgZAylgQ1B7ukislASxeq1zOVsohmFUWGoi42K8DWRP4j8fh8Ht3ZCOg2uYIBWJEvQKTl1ZA8ZCBTaCgRCSkK4qqtdlEOQZDZD"
+ */
